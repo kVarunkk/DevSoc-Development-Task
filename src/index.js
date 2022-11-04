d1c9033019b8a95e0da238d6e88910e126ebd5d1
kVarunkk <varunkumawatleap2@gmail.com>
2022-11-04 18:27:42 +0530

    update files

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/restaurant/:restaurantName" element={<Restaurant />} />
+      <Route path="/DevSoc-Development-Task" element={<App />} />
+      <Route path="/DevSoc-Development-Task/restaurant/:restaurantName" element={<Restaurant />} />
     </Routes>
   </BrowserRouter>
 );
